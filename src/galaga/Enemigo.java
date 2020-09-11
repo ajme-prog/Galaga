74a6351655e0975cb59a5961cb9d6650b7dec7df
Alan <alanmorataya19@gmail.com>
2020-09-10 22:19:27 -0600

    Creacion clase enemigo, y ventana principal

---
+++ src/galaga/Enemigo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package galaga;
+
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Alan
+ */
+public class Enemigo {
+    int posx=0;
+    int posy=0;
+    int vidas=2;
+    JLabel imagen;
+    
+    
+}
